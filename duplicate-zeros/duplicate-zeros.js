218fe93ea54440cedc695a0b0f397b70ae0380e6
GohDongwook <104189107+nada1221@users.noreply.github.com>
2024-10-12 01:26:13 +0900

    Time: 57 ms, Memory: 51.9 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+
+var duplicateZeros = function(arr) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 0 ){
+        arr.pop();
+        arr.splice(i+1,0,0);
+        i++;
+        }
+    }
+    
+};
